b5f2f2bea45476df11ba0612d9d668908718b80f
Zanderh32 <125381746+Zanderh32@users.noreply.github.com>
2023-09-13 14:19:58 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -4,49 +4,69 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 import time
+import threading
+
 PATH = "C:\\Program Files (x86)\\chromedriver.exe"
 
-driver = webdriver.Chrome()
-while(True):
-    driver.get("https://www.vype.com/Texas/Houston/vype-hou-public-school-cheer-team-of-the-year-fan-poll-presented-by"
-              "-freddys")
-    time.sleep(10)
 
-    # = driver.find_element(By.ID, "PDI_answer57332800")
+def vote():
+    driver = webdriver.Chrome()
 
+    driver.get(
+        "https://www.vype.com/Texas/Houston/vype-hou-public-school-cheer-team-of-the-year-fan-poll-presented-by"
+        "-freddys")
+    time.sleep(3)
 
-    #answer_text = "Tomball Cougars"
-    #tomball_cougars = answer_box.find_element(By.XPATH, f".//label[contains(., '{answer_text}')]/preceding-sibling::span/input")
+    while len(driver.find_elements(By.CLASS_NAME, "show-more")) == 0:
+        time.sleep(3)
 
-
-
-    link = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "show-more")))
+    link = driver.find_element(By.CLASS_NAME, "show-more")
     link.click()
 
+    while len(driver.find_elements(By.ID, "PDI_answer57332800")) == 0:
+        time.sleep(3)
+
     answer_box = driver.find_element(By.ID, "PDI_answer57332800")
-    time.sleep(7)
     answer_box.click()
 
-    vote_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "pd-vote-button12735729")))
-    time.sleep(7)
+    while len(driver.find_elements(By.ID, "pd-vote-button12735729")) == 0:
+        time.sleep(3)
+
+    vote_button = driver.find_element(By.ID, "pd-vote-button12735729")
     vote_button.click()
 
-    time.sleep(7)
+    if len(driver.find_elements(By.ID, "captcha_12735729")) > 0:
+        answer_sheet = driver.find_element(By.ID, "captcha_12735729")
+        numbers = answer_sheet.text.split('+')
+        num1 = int(numbers[0].strip())
+        numbers[1] = numbers[1].split("=")[0].strip()
+        num2 = int(numbers[1].strip())
+        result = num1 + num2
+        answer_box = driver.find_element(By.ID, "answer_12735729")
+        answer_box.send_keys(result)
+        time.sleep(1)
+        answer_box.send_keys(Keys.ENTER)
+        time.sleep(1)
 
+while(True):
+    t1 =threading.Thread(target=vote, args=())
+    t2 = threading.Thread(target=vote, args=())
+    t3 = threading.Thread(target=vote, args=())
+    t4 = threading.Thread(target=vote, args=())
+    t5 = threading.Thread(target=vote, args=())
+    t6 = threading.Thread(target=vote, args=())
+    t7 = threading.Thread(target=vote, args=())
+    t8 = threading.Thread(target=vote, args=())
 
-    if(len(driver.find_elements(By.ID, "captcha_12735729")) > 0):
-            answer_sheet = driver.find_element(By.ID, "captcha_12735729")
-            numbers = answer_sheet.text.split('+')
-            num1 = int(numbers[0].strip())
-            numbers[1] = numbers[1].split("=")[0].strip()
-            num2 = int(numbers[1].strip())
-            result = num1 + num2
-            answer_box = driver.find_element(By.ID, "answer_12735729")
-            answer_box.send_keys(result)
-            answer_box.send_keys(Keys.ENTER)
+    t1.start()
+    t2.start()
+    t3.start()
+    t4.start()
+    t5.start()
+    t6.start()
+    t7.start()
+    t8.start()
 
-    time.sleep(7)
-
-
+    t8.join()
 
 
