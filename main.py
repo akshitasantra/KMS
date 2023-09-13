4501a51385b6f1bb34afeed263c94094a833b224
Zanderh32 <125381746+Zanderh32@users.noreply.github.com>
2023-09-13 12:00:21 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -7,14 +7,44 @@ import time
 PATH = "C:\\Program Files (x86)\\chromedriver.exe"
 
 driver = webdriver.Chrome()
-driver.get("https://www.vype.com/Texas/Houston/vype-hou-public-school-cheer-team-of-the-year-fan-poll-presented-by"
-          "-freddys")
+while(True):
+    driver.get("https://www.vype.com/Texas/Houston/vype-hou-public-school-cheer-team-of-the-year-fan-poll-presented-by"
+              "-freddys")
+    time.sleep(5)
 
-try:
-    element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, "//span[text()='Read More']"))
-    )
-    element.click()
-except:
-    print("didnt work")
+    # = driver.find_element(By.ID, "PDI_answer57332800")
+
+
+    #answer_text = "Tomball Cougars"
+    #tomball_cougars = answer_box.find_element(By.XPATH, f".//label[contains(., '{answer_text}')]/preceding-sibling::span/input")
+
+
+
+    link = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "show-more")))
+    link.click()
+
+    answer_box = driver.find_element(By.ID, "PDI_answer57332800")
+    time.sleep(7)
+    answer_box.click()
+
+    vote_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "pd-vote-button12735729")))
+    time.sleep(7)
+    vote_button.click()
+
+    time.sleep(7)
+    answer_sheet = driver.find_element(By.ID, "captcha_12735729")
+    numbers = answer_sheet.text.split('+')
+    num1 = int(numbers[0].strip())
+    numbers[1] = numbers[1].split("=")[0].strip()
+    num2 = int(numbers[1].strip())
+    result = num1 + num2
+    answer_box = driver.find_element(By.ID, "answer_12735729")
+    answer_box.send_keys(result)
+    answer_box.send_keys(Keys.ENTER)
+
+
+    time.sleep(7)
+
+
+
 
