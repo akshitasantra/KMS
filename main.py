9b1883593c8499aeaed99db1bb0947b1af0b91e5
Zanderh32 <125381746+Zanderh32@users.noreply.github.com>
2023-09-13 21:55:52 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 import time
 import threading
@@ -10,32 +11,38 @@ PATH = "C:\\Program Files (x86)\\chromedriver.exe"
 
 
 def vote():
+
     driver = webdriver.Chrome()
 
     driver.get(
         "https://www.vype.com/Texas/Houston/vype-hou-public-school-cheer-team-of-the-year-fan-poll-presented-by"
         "-freddys")
-    time.sleep(3)
+
+
+    time.sleep(2)
 
     while len(driver.find_elements(By.CLASS_NAME, "show-more")) == 0:
-        time.sleep(3)
+        time.sleep(2)
 
     link = driver.find_element(By.CLASS_NAME, "show-more")
     link.click()
 
     while len(driver.find_elements(By.ID, "PDI_answer57332800")) == 0:
-        time.sleep(3)
+        time.sleep(2)
 
     answer_box = driver.find_element(By.ID, "PDI_answer57332800")
     answer_box.click()
 
     while len(driver.find_elements(By.ID, "pd-vote-button12735729")) == 0:
-        time.sleep(3)
+        time.sleep(2)
 
     vote_button = driver.find_element(By.ID, "pd-vote-button12735729")
     vote_button.click()
 
-    if len(driver.find_elements(By.ID, "captcha_12735729")) > 0:
+    time.sleep(2)
+
+    if len(driver.find_elements(By.ID, "captcha_12735729")) != 0:
+        print("working")
         answer_sheet = driver.find_element(By.ID, "captcha_12735729")
         numbers = answer_sheet.text.split('+')
         num1 = int(numbers[0].strip())
@@ -48,25 +55,9 @@ def vote():
         answer_box.send_keys(Keys.ENTER)
         time.sleep(1)
 
+    time.sleep(2)
+
 while(True):
-    t1 =threading.Thread(target=vote, args=())
-    t2 = threading.Thread(target=vote, args=())
-    t3 = threading.Thread(target=vote, args=())
-    t4 = threading.Thread(target=vote, args=())
-    t5 = threading.Thread(target=vote, args=())
-    t6 = threading.Thread(target=vote, args=())
-    t7 = threading.Thread(target=vote, args=())
-    t8 = threading.Thread(target=vote, args=())
-
+    t1 = threading.Thread(target=vote, args=())
     t1.start()
-    t2.start()
-    t3.start()
-    t4.start()
-    t5.start()
-    t6.start()
-    t7.start()
-    t8.start()
-
-    t8.join()
-
-
+    t1.join()
